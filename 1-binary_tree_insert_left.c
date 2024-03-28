f746821c0bdaa95844e8b2ac0e4eb822ba1bb7d8
Makarona10 <ahmedneyssi@gmail.com>
2024-03-29 00:35:27 +0200

    s

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,14 +17,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     if (!parent)
         return (NULL);
 
-    node = malloc(sizeof(binary_tree_t));
+    node = binary_tree_node(parent, value);
     if (!node)
         return (NULL);
 
     parent->left = node;
     node->parent = parent;
-    node->n = value;
-    node->left = NULL;
-    node->right = NULL;
     return (node);
 }
