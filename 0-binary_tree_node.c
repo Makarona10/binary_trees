af5e34bbb2f82637cc57d4f00e4a10133504a26f
Makarona10 <ahmedneyssi@gmail.com>
2024-03-28 18:25:24 +0200

    s

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,17 +12,17 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node;
+	binary_tree_t *node;
 
-    node = malloc(sizeof(binary_tree_t));
-    if (!node)
-    {
-        return (NULL);
-    }
-    node->parent = parent;
-    node->n = value;
-    node->right = NULL;
-    node->left = NULL;
-    
-    return node;
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
+	{
+		return (NULL);
+	}
+	node->parent = parent;
+	node->n = value;
+	node->right = NULL;
+	node->left = NULL;
+
+	return (node);
 }
