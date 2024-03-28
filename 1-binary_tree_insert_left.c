eaa78e6d98cb9f53f8eca17ebad79ba4ce392962
Makarona10 <ahmedneyssi@gmail.com>
2024-03-29 00:41:48 +0200

    s

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,9 +21,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     if (!node)
         return (NULL);
 
-    parent->left = node;
-    node->parent = parent;
+    node->left = parent->left;
     if (node->left)
         node->left->parent = node;
+    parent->left = node;
     return (node);
 }
