c489567f705d34f4755a8c36a8ce498cf0b1cccd
Makarona10 <ahmedneyssi@gmail.com>
2024-03-29 02:34:57 +0200

    s

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,14 +10,13 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (!tree)
-        return;
-    
-    if (tree->left)
-        binary_tree_delete(tree->left);
-    
-    if (tree->right)
-        binary_tree_delete(tree->right);
-    
-    free(tree);
+	if (!tree)
+		return;
+
+	if (tree->left)
+		binary_tree_delete(tree->left);
+
+	if (tree->right)
+		binary_tree_delete(tree->right);
+	free(tree);
 }
