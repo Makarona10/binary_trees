65fbb0993ffc0a618cf778e60c6c44cb045d7ef8
Makarona10 <ahmedneyssi@gmail.com>
2024-03-29 13:38:45 +0200

    s

--- main.c
+++ main.c
@@ -8,6 +8,10 @@ int main ()
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
     root->right = binary_tree_node(root, 402);
+    root->left->left = binary_tree_node(root->left, 6);
+    root->left->right = binary_tree_node(root->left, 56);
+    root->right->left = binary_tree_node(root->right, 256);
+    root->right->right = binary_tree_node(root->right, 512);
     x = binary_tree_height(root);
     printf("%lu\n", x);
 }
