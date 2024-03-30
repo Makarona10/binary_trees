870ae35fbd42b1db387c77adc5cb60aea304bcfe
Makarona10 <ahmedneyssi@gmail.com>
2024-03-30 03:41:21 +0200

    s

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ *
+ * @node: a pointer to the node to find the uncle
+ *
+ * Return: If node is NULL, return NULL
+ * If node has no uncle, return NULL
+*/
+
