506c9d58401de53c2bde7774ae4dcb5aae404e5f
Makarona10 <ahmedneyssi@gmail.com>
2024-03-30 03:54:08 +0200

    s

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,15 +10,15 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t l, r;
+	size_t l, r;
 
 	if (!tree || (tree && !tree->left && !tree->right))
 		return (0);
 
-    l = binary_tree_height(tree->left);
+	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
-	
-    if (l >= r)
+
+	if (l >= r)
 		return (1 + l);
 
 	return (1 + r);
