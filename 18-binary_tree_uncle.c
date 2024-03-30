fbb3015ca1f46c777caa117a07fa4fbf4a160344
Makarona10 <ahmedneyssi@gmail.com>
2024-03-30 03:40:22 +0200

    s

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
         return (NULL);
 
     if (node->parent == node->parent->parent->left)
-        return (node->parent->parent->right)
+        return (node->parent->parent->right);
     else
         return (node->parent->parent->left);
 }
